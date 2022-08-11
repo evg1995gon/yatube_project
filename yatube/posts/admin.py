3ebbe730359a1784f048fb1ea1719ef4d47718a9
Евгений Гончаров <evg1995gon@gmail.com>
2022-08-11 01:24:11 -0700

    Admin Zone and Models

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
@@ -13,4 +10,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
 
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Post
 
 # Главная страница
 def index(request):
     template = 'posts/index.html'
     title = 'Главная страница'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
-        'text': "Это главная страница проекта Yatube"
+        'text': "Это главная страница проекта Yatube",
+        'posts': posts
     }
     return render(request, template, context)
 
