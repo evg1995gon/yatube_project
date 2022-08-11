a87bcebc9b5c17c56097006243d5e2dd02785b41
Евгений Гончаров <evg1995gon@gmail.com>
2022-08-11 00:44:41 -0700

    Admin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('text', 'pub_date', 'author')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+
+
+admin.site.register(Post)
